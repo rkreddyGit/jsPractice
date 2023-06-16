d45cb7f3dd1aec7b6e7cf984b075457c9529c532
puneethvadem <122425893+puneethvadem@users.noreply.github.com>
2023-06-16 18:20:03 +0530

    Update operations.js

--- operations.js
+++ operations.js
@@ -1,6 +1,24 @@
-let qw = 5;
-let er = 20;
+// We already know how to create variables and how to assign them numbers.
+//Now, we will learn how to compare these variables and their values with each other. We can do so by using comparison operators.
+//The first operator we explore is the Equal to (==) operator. It checks if two values are equal. For example:
 
-let ans = qw + re;
+2 == 2;
 
-console.log(ans);
+// Can you guess what the output will be?
+
+const priceOne = 5;
+const priceTwo = 10;
+console.log(priceOne == priceTwo);
+
+// Here, we used the Equal to (==) operator to check if the values of priceOne and priceTwo are equal.
+
+// Comparison operators always return a boolean value. In this case we got false because priceOne and priceTwo are not equal.
+
+// Now, priceOne and priceTwo have the same value. Let's see what the output will be:
+
+const priceOne = 10;
+const priceTwo = 10;
+console.log(priceOne == priceTwo);
+
+// The second operator we explore is the Not Equal to (!=) operator. It checks if two values are not equal. For example:
+2 != 2;
