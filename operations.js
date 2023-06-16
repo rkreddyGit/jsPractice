50d8f9310c306a42c78a64a82fcdbe2b4a2ce0e9
puneethvadem <122425893+puneethvadem@users.noreply.github.com>
2023-06-16 18:30:44 +0530

    Update operations.js

--- operations.js
+++ operations.js
@@ -22,3 +22,44 @@ console.log(priceOne == priceTwo);
 
 // The second operator we explore is the Not Equal to (!=) operator. It checks if two values are not equal. For example:
 2 != 2;
+
+// In the console.log() statement below we use the Equal operator to check whether numOne and numTwo have the same value. Change the code so that the console.log() statement logs true.
+
+const numOne = 5;
+const numTwo = 5;
+console.log(numOne == numTwo);
+
+// In the console.log() statement below we use the Not Equal operator to check whether numOne and numTwo have different values. Change the code so that the console.log() statement logs true.
+const numOne = 5;
+const numTwo = 6;
+console.log(numOne != numTwo);
+
+// Let's look at other types of comparison operators.
+
+// The Greater than (>) operator checks if the left value is greater than the right value. For example:
+
+2 > 1; // checks if 2 is greater than 1
+
+// Can you guess what the output will be?
+
+const priceOne = 5;
+const priceTwo = 10;
+
+console.log(priceOne > priceTwo);
+console.log(priceTwo > priceOne);
+
+// The Less than (<) operator checks if the left value is less than the right value. For example:
+2 < 1; // checks if 2 is less than 1
+
+// Let's use the Less than operator in the example for above:
+
+const priceOne = 5;
+const priceTwo = 10;
+
+console.log(priceOne < priceTwo);
+console.log(priceTwo < priceOne);
+
+// Similar to the operators above, there are also the Less than or equal (<=) and Greater than or equal (>=) operators. For example:
+
+2 <= 1; // checks if 2 is less than or equal 1
+2 >= 1; // checks if 2 is greater than or equal 1
